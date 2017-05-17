e89c16c81c39bc9414b2a85b48386a3047660bca
Vorsicht <evgheniyhome@gmail.com>
2017-05-17 12:54:38 +0300

    Change game  to lower screen resolution

--- core/src/com/mygdx/game/GameManager.java
+++ core/src/com/mygdx/game/GameManager.java
@@ -8,10 +8,10 @@ public final class GameManager {
 
     public static final short WALL = 1 << 1;
 
-    public static float PPM = 5f;
+    public static float PPM = 16;
 
-    public static int WORLD_WIDTH = 800;
-    public static int WORLD_HEIGHT = 600;
+    public static int WORLD_WIDTH = 50;
+    public static int WORLD_HEIGHT = 50;
 
 
 }
