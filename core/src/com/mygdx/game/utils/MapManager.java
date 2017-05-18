02898b8b9724d37fb7b6d541afd93d86b9e35428
Vorsicht <evgheniyhome@gmail.com>
2017-05-18 10:17:46 +0300

    Removed unused fields

--- core/src/com/mygdx/game/utils/MapManager.java
+++ core/src/com/mygdx/game/utils/MapManager.java
@@ -1,8 +1,10 @@
 package com.mygdx.game.utils;
 
 
+import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.mygdx.game.GameManager;
 
 /**
  * Created by Evgheniy on 5/15/2017.
@@ -12,14 +14,27 @@ public enum MapManager {
     INSTANCE;
 
     public TiledMap map;
-    public static int width;
-    public static int height;
+    public static int mapWidth;
+    public static int mapHeight;
+
+    public static int tilePixelHeight;
+    public static int tilePixelWidth;
+    public static float mapPixelWidth;
+    public static float mapPixelHeight;
 
     public void loadMap(String mapPath) {
         map = new TmxMapLoader().load(mapPath);
-        width = map.getProperties().get("width", Integer.class);
-        height = map.getProperties().get("height", Integer.class);
+        MapProperties mapProperties = map.getProperties();
 
+        mapWidth = mapProperties.get("width", Integer.class);
+        mapHeight = mapProperties.get("height", Integer.class);
+
+
+        tilePixelHeight = mapProperties.get("tileheight", Integer.class);
+        tilePixelWidth = mapProperties.get("tilewidth", Integer.class);
+
+        mapPixelWidth = (mapWidth * tilePixelWidth) / GameManager.PPM;
+        mapPixelHeight = (mapHeight * tilePixelHeight) / GameManager.PPM;
 
     }
 
