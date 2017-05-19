6c4c6bd5dcb506f814ca3f24b3101b097a8048d2
Vorsicht <evgheniyhome@gmail.com>
2017-05-19 10:51:22 +0300

    Adding new category bits

--- core/src/com/mygdx/game/GameManager.java
+++ core/src/com/mygdx/game/GameManager.java
@@ -6,7 +6,12 @@ package com.mygdx.game;
 
 public final class GameManager {
 
+
+    public static short EMPTY_BIT = 0;
     public static final short WALL = 1 << 1;
+    public static final short ACTOR = 2 << 1;
+    public static final short TARGET = 3 << 1;
+
 
     public static float PPM = 16;
 
